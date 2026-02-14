92f7859a80ed688a90b36c783d2d579911b2f264
maira155 <maigecalram1505@gmail.com>
2026-02-14 18:00:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 function comprar(nombre) {
     alert("Has comprado: " + nombre);
 }
+
+//login
+function abrirLogin() {
+    document.getElementById("loginModal").style.display = "flex";
+}
+
+function cerrarLogin() {
+    document.getElementById("loginModal").style.display = "none";
+}
+
+function iniciarSesion(event) {
+    event.preventDefault();
+    alert("Inicio de sesión exitoso");
+    cerrarLogin();
+}
+
+window.onclick = function(event) {
+    const modal = document.getElementById("loginModal");
+    if (event.target === modal) {
+        cerrarLogin();
+    }
+};
